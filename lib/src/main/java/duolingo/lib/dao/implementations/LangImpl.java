e3d70fa69ea10bd31f6b151722625be2b5e2b1db
susom <susom@Susomatuso-Portatil>
2020-11-30 16:58:35 +0100

    Added getAllLangs on LangImpl

--- lib/src/main/java/duolingo/lib/dao/implementations/LangImpl.java
+++ lib/src/main/java/duolingo/lib/dao/implementations/LangImpl.java
@@ -3,6 +3,10 @@ package duolingo.lib.dao.implementations;
 import duolingo.lib.dao.interfaces.ILang;
 import duolingo.lib.model.LangModel;
 import duolingo.lib.model.UsersModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -12,32 +16,40 @@ public class LangImpl implements ILang {
 
 	@Override
 	public LangModel getLangById(int id) {
-		try( Session session = HibernateUtil.getSessionFactory().openSession() )
-        { 
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
 			LangModel lang = (LangModel) session.get(LangModel.class, id);
 			return lang;
-        }catch(Exception e) {        	
-            e.printStackTrace();
-            return null;
-        }
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	@Override
 	public boolean saveLang(LangModel lang) {
 		Transaction t = null;
-        try( Session session = HibernateUtil.getSessionFactory().openSession() )
-        {            
-            t = session.beginTransaction();
-            session.save(lang);
-            t.commit();
-            return true;
-            
-        }catch(Exception e) {        	
-            e.printStackTrace();
-            return false;
-        }
-		
-		
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			t = session.beginTransaction();
+			session.save(lang);
+			t.commit();
+			return true;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	@Override
+	public ArrayList<LangModel> getAllLang() {
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			List<LangModel> lang = session.createCriteria(LangModel.class).list();
+
+			return new ArrayList<LangModel>(lang);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 }
--- lib/src/main/java/duolingo/lib/dao/interfaces/ILang.java
+++ lib/src/main/java/duolingo/lib/dao/interfaces/ILang.java
@@ -1,10 +1,13 @@
 package duolingo.lib.dao.interfaces;
 
 
+import java.util.ArrayList;
+
 import duolingo.lib.model.LangModel;
 
 public interface ILang {
 
 	public LangModel getLangById(int id);
 	public boolean saveLang(LangModel lang);
+	public ArrayList <LangModel>  getAllLang();
 }
